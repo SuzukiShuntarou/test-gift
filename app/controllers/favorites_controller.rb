a66518412aa01a36be99d37427bd9d60b26a892e
SuzukiShuntarou <da8801593@gmail.com>
2025-01-28 21:56:44 +0900

    いいね機能をturbo-frameで実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,6 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = Favorite.find(params[:id])
-    favorite.increment!(:good_count)
-    redirect_to request.referer
+    @favorite = Favorite.find(params[:id])
+    @favorite.increment!(:good_count)
   end
 end
