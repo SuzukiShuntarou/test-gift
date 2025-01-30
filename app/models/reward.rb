f95f6047cea8ee7ff2ea479dfa2892c520ca2657
SuzukiShuntarou <da8801593@gmail.com>
2025-01-30 22:19:03 +0900

    完了日を過ぎたら招待できないように変更

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -6,7 +6,7 @@ class Reward < ApplicationRecord
   accepts_nested_attributes_for :goals, allow_destroy: true
 
   def invite(current_user)
-    unless self.users.include?(current_user)
+    if self.in_progress? && !self.users.include?(current_user)
       self.users << current_user
 
       # 初期目標の作成
