fdd2e409c8240bd0d95a9a388d0520a878ceaa5a
SuzukiShuntarou <da8801593@gmail.com>
2025-02-02 00:31:41 +0900

    fixtureとsystemテストを作成

--- test/system/reward_test.rb
+++ test/system/reward_test.rb
@@ -8,8 +8,16 @@ class RewardsTest < ApplicationSystemTestCase
     assert_text 'Log in'
     fill_in 'Email', with: 'alice@example.com'
     fill_in 'Password', with: 'password'
-    click_on 'Log in'
+    click_link_or_button 'Log in'
     assert_text 'Signed in successfully.'
+  end
+
+  def visit_reward_path(reward)
+    visit rewards_path
+    assert_text 'Rewards#index'
+    click_link_or_button '実施中'
+    click_link_or_button "#{reward.location}で#{reward.content}する"
+    assert_text 'Rewards#show'
   end
 
   test 'visiting rewards index in progress and completed' do
@@ -19,7 +27,95 @@ class RewardsTest < ApplicationSystemTestCase
     assert_text '実施中'
     assert_text '叙々苑で食事する'
 
-    click_on '完了'
+    click_link_or_button '完了'
     assert_text '草津で温泉旅行する'
+  end
+
+  test 'visiting rewards show in progress' do
+    reward = rewards(:alice_reward_in_progress)
+    goal = goals(:alice_goal_in_progress)
+    liking = favorites(:alice_liking_in_progress)
+    cheering = cheerings(:alice_cheering_in_progress)
+
+    visit_reward_path(reward)
+
+    within('#reward') do
+      assert_text "#{reward.completiondate}に"
+      assert_text "#{reward.location}で"
+      assert_text "#{reward.content}する"
+    end
+
+    within("div##{dom_id(goal)}") do
+      assert_text "ユーザ名：#{goal.user.name}"
+      assert_text "目標：#{goal.content}"
+      assert_text "進捗率：#{goal.progress}%"
+    end
+    within("div##{dom_id(liking)}") do
+      assert_text liking.good_count
+    end
+    within("div##{dom_id(cheering)}") do
+      assert_text cheering.cheering_count
+    end
+  end
+
+  test 'should create rewards and goal' do
+    visit rewards_path
+    assert_text 'Rewards#index'
+    click_link_or_button '新規作成'
+
+    within("form[action='/rewards']") do
+      fill_in 'reward[completiondate]', with: Date.current.tomorrow
+      fill_in 'reward[location]', with: '叙々苑'
+      fill_in 'reward[content]', with: '焼肉'
+      fill_in 'reward[goals_attributes][0][content]', with: '毎日早寝早起きする'
+      fill_in 'reward[goals_attributes][0][progress]', with: 20
+      click_link_or_button 'Create Reward'
+    end
+
+    assert_text 'ご褒美の登録に成功！'
+    assert_text "#{Date.current.tomorrow}に"
+    assert_text '叙々苑で'
+    assert_text '焼肉する'
+    assert_text '目標：毎日早寝早起きする'
+    assert_text '進捗率：20%'
+  end
+
+  test 'should edit reward in progress' do
+    reward = rewards(:alice_reward_in_progress)
+    goals(:alice_goal_in_progress)
+
+    visit_reward_path(reward)
+
+    within('#reward') do
+      assert_selector 'a', text: '編集'
+      click_link_or_button '編集'
+    end
+    within('.modal-body') do
+      fill_in 'reward_location', with: '草津'
+      fill_in 'reward_content', with: '温泉旅行'
+      click_link_or_button '保存'
+    end
+
+    assert_text '草津で'
+    assert_text '温泉旅行する'
+  end
+
+  test 'should edit goal in progress' do
+    reward = rewards(:alice_reward_in_progress)
+    goal = goals(:alice_goal_in_progress)
+
+    visit_reward_path(reward)
+
+    within("div##{dom_id(goal)}") do
+      assert_selector 'a', text: '編集'
+      click_link_or_button '編集'
+    end
+    within('.modal-body') do
+      fill_in 'goal_content', with: 'ランニングする'
+      fill_in 'goal_progress', with: '99'
+      click_link_or_button '保存'
+    end
+    assert_text '目標：ランニングする'
+    assert_text '進捗率：99%'
   end
 end
